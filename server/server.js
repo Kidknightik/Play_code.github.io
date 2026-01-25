7f83b2ee6fb2664abb71a77d9bc76bcf1df4880a
Kidknightik <fewpewolywgrthsugb@gmail.com>
2026-01-25 15:54:38 +0300

    tying fix health check on render

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).send("API is running");
+});
+
 const products = [
   {
     id: 1,
@@ -19,28 +23,11 @@ const products = [
     price: 1100,
     category: "phones",
     image: "https://via.placeholder.com/300x200?text=Galaxy+S24"
-  },
-  {
-    id: 3,
-    title: "MacBook Pro 14",
-    price: 2500,
-    category: "laptops",
-    image: "https://via.placeholder.com/300x200?text=MacBook+Pro"
   }
 ];
 
-// получить все товары
 app.get("/products", (req, res) => {
   res.json(products);
-});
-
-// получить товар по id
-app.get("/products/:id", (req, res) => {
-  const product = products.find(p => p.id === Number(req.params.id));
-  if (!product) {
-    return res.status(404).json({ message: "Product not found" });
-  }
-  res.json(product);
 });
 
 const PORT = process.env.PORT || 3000;
